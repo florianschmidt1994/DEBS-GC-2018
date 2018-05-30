ffe0713abbbad395bd927f1c68b135396e5b083c
André <mail@andremartin.de>
2018-05-30 20:04:01 +0200

    Integrated Oleh's new model

---
+++ solution/flask-main-new.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import os
+import numpy as np
+import pandas as pd
+import h5py
+from flask import Flask, request
+from sklearn.externals import joblib
+from tensorflow.python.keras.models import load_model
+from solution2.Predictor import Predictor
+import sys
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+app = Flask(__name__)
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+port_path = os.path.join(current_dir, "datasets/ports_extended.csv")
+filename1 = os.path.join(current_dir, "new_models/destination_ensemble.pkl")
+filename2 = os.path.join(current_dir, "new_models/clusters.pkl")
+filename3 = os.path.join(current_dir, "new_models/scaler.pkl")
+filename4 = os.path.join(current_dir, "new_models/eta_mlp.h5")
+
+port_predictor = joblib.load(filename1)
+clusters = joblib.load(filename2)
+scaler = joblib.load(filename3)
+time_predictor = load_model(filename4) 
+
+# create predictor and system adapter
+predictor = Predictor(port_predictor, time_predictor)
+
+@app.route("/", methods=['GET'])
+def index():
+    return "HEALTHY"
+
+
+@app.route("/predict_port", methods=['POST'])
+def predict_port():
+    input = str(request.data)
+    port, time = predictor.predict_port_and_time(input)
+    return port
+
+@app.route("/predict_port_and_time", methods=['POST'])
+def predict_port_and_time():
+    input = str(request.data)
+    port, time = predictor.predict_port_and_time(input)
+    return "%s, %s" % (port, time)
+
+port = int(sys.argv[1])
+app.run(host='0.0.0.0',port=port)
---
+++ solution/solution2/Predictor.py
@@ -0,0 +1,233 @@
+import numpy as np
+import pandas as pd
+import datetime
+import time
+
+ports = {'ALEXANDRIA': 0,
+ 'AUGUSTA': 1,
+ 'BARCELONA': 2,
+ 'CAGLIARI': 3,
+ 'CARTAGENA': 4,
+ 'CASTELLON': 5,
+ 'CEUTA': 6,
+ 'DAMIETTA': 7,
+ 'DILISKELESI': 8,
+ 'FOS SUR MER': 9,
+ 'FORMIA': 10,
+ 'GEMLIK': 11,
+ 'GENOVA': 12,
+ 'GIBRALTAR': 13,
+ 'JIJEL': 14,
+ 'HAIFA': 15,
+ 'IBIZA': 16,
+ 'ISKENDERUN': 17,
+ 'LAPSEKI': 18,
+ 'LIVORNO': '19',
+ 'LIMASSOL': 20,
+ 'MARSAXLOKK': 21,
+ 'MONACO': 22,
+ 'NEMRUT': 23,
+ 'PALERMO': 24,
+ 'PALMA DE MALLORCA': 25,
+ 'PIRAEUS': 26,
+ 'PIOMBINO': 27,
+ 'PLOCE': 28,
+ 'PORT SAID': 29,
+ 'SANT ANTONI': 30,
+ 'GIOIA TAURO': 31,
+ 'SKIKDA': 32,
+ 'ORAN ANCH': 33,
+ 'TARRAGONA': 34,
+ 'TEL AVIV-YAFO': 35,
+ 'TOBRUK': 36,
+ 'TRAPANI': 37,
+ 'TRIPOLIS': 38,
+ 'TUZLA': 39,
+ 'VALENCIA': 40,
+ 'VALLETTA': 41,
+ 'YALOVA': 42}
+
+
+coordinates = {
+ 0: np.array([ 29.88,  31.18]),
+ 1: np.array([ 15.21,  37.2 ]),
+ 2: np.array([  2.16,  41.35]),
+ 3: np.array([  9.11,  39.21]),
+ 4: np.array([ -0.97,  37.58]),
+ 5: np.array([  2.00000000e-02,   3.99700000e+01]),
+ 6: np.array([ -5.31,  35.89]),
+ 7: np.array([ 31.76,  31.47]),
+ 8: np.array([ 29.53,  40.77]),
+ 9: np.array([  4.87,  43.42]),
+ 10: np.array([ 13.61,  41.26]),
+ 11: np.array([ 29.12,  40.43]),
+ 12: np.array([  8.91,  44.4 ]),
+ 13: np.array([ -5.36,  36.14]),
+ 14: np.array([  5.78,  36.82]),
+ 15: np.array([ 35.  ,  32.83]),
+ 16: np.array([  1.44,  38.91]),
+ 17: np.array([ 36.18,  36.68]),
+ 18: np.array([ 26.69,  40.35]),
+ 19: np.array([ 10.31,  43.56]),
+ 20: np.array([ 33.03,  34.66]),
+ 21: np.array([ 14.54,  35.83]),
+ 22: np.array([  7.43,  43.74]),
+ 23: np.array([ 26.9 ,  38.77]),
+ 24: np.array([ 13.37,  38.13]),
+ 25: np.array([  2.63,  39.56]),
+ 26: np.array([ 23.61,  37.95]),
+ 27: np.array([ 10.55,  42.93]),
+ 28: np.array([ 17.44,  43.04]),
+ 29: np.array([ 32.32,  31.24]),
+ 30: np.array([  1.3 ,  38.98]),
+ 31: np.array([ 15.9 ,  38.45]),
+ 32: np.array([  6.92,  36.89]),
+ 33: np.array([ -0.63,  35.75]),
+ 34: np.array([  1.22,  41.1 ]),
+ 35: np.array([ 34.77,  32.09]),
+ 36: np.array([ 23.98,  32.07]),
+ 37: np.array([ 12.51,  38.01]),
+ 38: np.array([ 13.18,  32.9 ]),
+ 39: np.array([ 29.29,  40.83]),
+ 40: np.array([ -0.32,  39.44]),
+ 41: np.array([ 14.52,  35.89]),
+ 42: np.array([ 29.48,  40.72])
+}
+
+
+
+SHIP_ID = 0  # 0xc35c9ebbf48cbb5857a868ce441824d0b2ff783a
+SHIPTYPE = 1  # 99
+SPEED = 2  # 8.2
+LON = 3  # 14.56034
+LAT = 4  # 35.8109
+COURSE = 5  # 109
+HEADING = 6  # 511
+TIMESTAMP = 7  # 10-03-15 12:15
+DEPARTURE_PORT_NAME = 8  # MARSAXLOKK
+REPORTED_DRAUGHT = 9  # ''
+#TRIP_ID = 10  # 0xc35c9_10-03-15 12:xx - 10-03-15 13:26
+ARRIVAL_CALC = 10  # ??
+ARRIVAL_PORT_CALC = 11  # ??
+
+
+class Predictor:
+    def __init__(self, port_model, time_model):
+        self.port_model = port_model
+        self.time_model = time_model
+        self.ports = ports
+
+    def predict_port_and_time(self, data):
+        # Parse string to array and convert all fields to numbers
+
+        #coordinates = get_coordinates() #Preloading coordinates for each port
+
+        feed_list = parse(data)
+        initial_time = feed_list[6]  # we need this below. TODO: Fix this ugly entanglement
+
+        port_features = feed_list[:6]+feed_list[7:]
+        port_features = np.asarray(port_features)
+
+        # Predict destination port
+        port = self.port_model.predict(port_features.reshape(-1, 8))
+        port = port[0]
+        #output for Q1
+        destination_port = int_to_port(port)
+
+        ''' Order of features should be
+        [ 'SHIPTYPE', 'SPEED', 'LON_x', 'LAT_x', 'COURSE', 'HEADING',
+        'DEPARTURE_PORT_NAME', 'REPORTED_DRAUGHT',
+        'ARRIVAL_PORT_CALC',  'LON_y', 'LAT_y', 'DISTANCE']
+        '''
+
+        # Select our input features for time prediction and append the predicted destination port to them
+        feed_tuple = np.append(feed_list, port)
+        feed_tuple = np.append(feed_tuple, coordinates.get(port))
+        feed_tuple = np.asarray([round(float(i),2) if '.' in i else int(i) for i in feed_tuple])
+        distance = haversine_np(feed_tuple[1], feed_tuple[2], feed_tuple[-2], feed_tuple[-1])
+        feed_tuple = np.append(feed_tuple, distance)
+
+        # Predict the ETA
+        time_left = self.time_model.predict(scaler.transform(feed_tuple.reshape(1, -1)))
+        #eta = initial_time + time_left
+
+        return destination_port, feed_tuple, time_left#from_unixtime(eta)
+
+
+def parse(input_str):
+    data = input_str.split(",")
+    parsed_data = [
+        data[SHIPTYPE],
+        data[SPEED],
+        data[LON],
+        data[LAT],
+        data[COURSE],
+        data[HEADING],
+        to_unixtime(data[TIMESTAMP]),
+        port_to_int(data[DEPARTURE_PORT_NAME]),
+        data[REPORTED_DRAUGHT]
+    ]
+    return [0 if elem is '' else elem for elem in parsed_data]
+
+def predictQ2(parsed_tuple, port):
+    '''
+    [ 'SHIPTYPE', 'SPEED', 'LON_x', 'LAT_x', 'COURSE', 'HEADING',
+        'DEPARTURE_PORT_NAME', 'REPORTED_DRAUGHT',
+        'ARRIVAL_PORT_CALC',  'LON_y', 'LAT_y', 'DISTANCE'
+        ]
+    '''
+    parsed_tuple = np.append(parsed_tuple,port)
+    parsed_tuple = np.append(parsed_tuple,coordinates.get(port))
+    #np.append(feed_tuple,coordinates.get(port[0]))
+    #check assert
+
+    time_left = time_predictor.predict(parsed_tuple.reshape(-1, 12))
+    #pd.to_datetime(data[-3]) + datetime.timedelta(minutes=time_left[0])
+
+    timestamp = to_unixtime(data[-3])
+    eta = timestamp + time_left
+    port = unparse(port)
+    return port, from_unixtime(eta)
+
+
+def port_to_int(port_name):
+    return ports.get(port_name)
+
+
+def int_to_port(data):
+    for key, value in ports.items():
+        if data == value:
+            return key
+
+
+def to_unixtime(value):
+    try:
+        return time.mktime(time.strptime(value, "%d-%m-%y %H:%M"))
+    except:
+        try:
+            return time.mktime(time.strptime(value, "%d-%m-%Y %H:%M"))
+        except:
+            raise Exception("Unable to parse date '%s'" % value)
+
+
+def from_unixtime(time):
+    return datetime.datetime.fromtimestamp(time).strftime('%d-%m-%Y %H:%M')
+
+def haversine_np(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+
+    All args must be of equal length.
+
+    """
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = np.sin(dlat/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2.0)**2
+
+    c = 2 * np.arcsin(np.sqrt(a))
+    km = 6367 * c
+    return km

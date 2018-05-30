414fb5c78d99a58961d794b84f4983b1daf35f70
André Martin <mail@andremartin.de>
2018-05-30 12:05:52 +0200

    Made docker images consistent

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,6 @@ RUN pip3 install --no-build-isolation scipy pandas bitstring 'scikit-learn==0.18
 RUN pip3 install --no-build-isolation flask
 RUN mkdir -p /usr/src/debs2018solution 
 WORKDIR /usr/src/debs2018solution 
-RUN echo "Test1"
 ADD target/debs_2018_gc_sample_system-1.0.jar /usr/src/debs2018solution 
 ADD solution/ /usr/src/debs2018solution/solution
 CMD java -cp debs_2018_gc_sample_system-1.0.jar org.hobbit.core.run.ComponentStarter org.hobbit.debs_2018_gc_samples.System.SystemAdapter
--- src/main/java/org/hobbit/debs_2018_gc_samples/SampleDockersBuilder.java
+++ src/main/java/org/hobbit/debs_2018_gc_samples/SampleDockersBuilder.java
@@ -20,11 +20,14 @@ public class SampleDockersBuilder extends DynamicDockerFileBuilder {
 
         String dockerfile = "" +
             "FROM korekontrol/ubuntu-java-python3 \n" +
-            "RUN pip install 'scikit-learn==0.18.1' pika numpy scipy pandas bitstring \n" +
+            "RUN pip3 install --upgrade pip \n" +
+            "RUN pip3 install pika numpy \n" +
+            "RUN pip3 install --no-build-isolation scipy pandas bitstring 'scikit-learn==0.18.1' \n" +
+            "RUN pip3 install --no-build-isolation flask \n" +
             "RUN mkdir -p /usr/src/debs2018solution \n" +
             "WORKDIR /usr/src/debs2018solution \n" +
             "ADD target/debs_2018_gc_sample_system-1.0.jar /usr/src/debs2018solution \n" +
-            "ADD solution/ /usr/src/debs2018solution \n" +
+            "ADD solution/ /usr/src/debs2018solution/solution \n" +
             "CMD java -cp debs_2018_gc_sample_system-1.0.jar org.hobbit.core.run.ComponentStarter org.hobbit.debs_2018_gc_samples.System.SystemAdapter";
 
         buildDirectory(".");
